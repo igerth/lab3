2c9cbe493cb7c83fe996e70ace1b32b03d8e9a34
igerth <122569790+igerth@users.noreply.github.com>
2023-01-26 12:58:02 -0800

    Made changes to ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,6 +7,9 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+    int[] myInput = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(myInput);
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, myInput);
 	}
 
 
@@ -14,5 +17,9 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] myInput = {1, 2, 3, 4, 5};
+    ArrayExamples.reversed(myInput);
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, myInput);
   }
+
 }
